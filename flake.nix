6749d13eac15ade6dda0df918b1fc64fd472abec
Wout Mertens <Wout.Mertens@gmail.com>
2024-05-02 08:17:42 +0200

    actually use home-manager

--- flake.nix
+++ flake.nix
@@ -20,6 +20,13 @@
       };
     in
     {
+      # Our overlays
+      overlays.default = (final: prev: {
+        wout-scripts = final.callPackage ./home/wout-scripts.nix { };
+        google-chrome = prev.google-chrome.override { commandLineArgs = "--enable-features=TouchpadOverscrollHistoryNavigation"; };
+        home-manager = let hm = home-manager.packages.${system}.default; in prev.writeScriptBin "home-manager" ''${hm}/bin/home-manager --flake ${self.outPath} "$@"'';
+      });
+
       # NixOS configuration
       nixosConfigurations.wmertens-nixos = nixpkgs.lib.nixosSystem {
         inherit system;
@@ -53,29 +60,27 @@
       };
 
       # Home-manager
-      homeConfigurations = {
-        wmertens = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            {
-              # home-manager config
-              home = {
-                inherit username homeDirectory;
-                stateVersion = "22.05";
+      homeConfigurations.wmertens = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          {
+            # home-manager config
+            home = {
+              inherit username homeDirectory;
+              stateVersion = "22.05";
+            };
+            nix.registry.N =
+              {
+                from = { type = "indirect"; id = "N"; };
+                flake = nixpkgs;
               };
-              nix.registry.N =
-                {
-                  from = { type = "indirect"; id = "N"; };
-                  flake = nixpkgs;
-                };
-            }
+          }
 
-            ./home
-          ];
-        };
+          ./home
+        ];
       };
 
-      # allow `nix run . switch` to work as home-manager
-      packages.x86_64-linux.default = home-manager.packages.x86_64-linux.default;
+      # allow `nix run . switch` to work as home-manager with the current flake
+      packages.${system}.default = pkgs.home-manager;
     };
 }
