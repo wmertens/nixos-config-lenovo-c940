d4ca3541f80a2b7bc423c5a7d80b7165f1a1908a
Wout Mertens <Wout.Mertens@gmail.com>
2024-11-19 08:29:36 +0100

    fonts: macOS like rendering

--- fonts.nix
+++ fonts.nix
@@ -1,42 +1,58 @@
 { config, pkgs, ... }:
 
 {
+  # Tune font rendering like macOS
+  environment.variables = {
+    # Enable stem darkening
+    FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0";
+  };
+
   fonts = {
-    # fontconfig = {
-    #   localConf = ''
-    #     <?xml version="1.0"?>
-    #     <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-    #     <fontconfig>
-    #       <alias binding="weak">
-    #         <family>monospace</family>
-    #         <prefer>
-    #           <family>emoji</family>
-    #         </prefer>
-    #       </alias>
-    #       <alias binding="weak">
-    #         <family>sans-serif</family>
-    #         <prefer>
-    #           <family>emoji</family>
-    #         </prefer>
-    #       </alias>
-    #       <alias binding="weak">
-    #         <family>serif</family>
-    #         <prefer>
-    #           <family>emoji</family>
-    #         </prefer>
-    #       </alias>
-    #     </fontconfig>
-    #           '';
-    #   defaultFonts = {
-    #     emoji = [ "Noto Color Emoji" ];
-    #     monospace = [ "FreeMono" ];
-    #     sansSerif = [ "FreeSans" ];
-    #     serif = [ "FreeSerif" ];
-    #   };
-    # };
+    fontconfig = {
+      # no subpixel tricks, we're HiDPI
+      subpixel = {
+        lcdfilter = "none";
+        rgba = "none";
+      };
+      # slight hinting and antialiasing
+      hinting.style = "slight";
+      antialias = true;
+
+      #   localConf = ''
+      #     <?xml version="1.0"?>
+      #     <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+      #     <fontconfig>
+      #       <alias binding="weak">
+      #         <family>monospace</family>
+      #         <prefer>
+      #           <family>emoji</family>
+      #         </prefer>
+      #       </alias>
+      #       <alias binding="weak">
+      #         <family>sans-serif</family>
+      #         <prefer>
+      #           <family>emoji</family>
+      #         </prefer>
+      #       </alias>
+      #       <alias binding="weak">
+      #         <family>serif</family>
+      #         <prefer>
+      #           <family>emoji</family>
+      #         </prefer>
+      #       </alias>
+      #     </fontconfig>
+      #           '';
+      #   defaultFonts = {
+      #     emoji = [ "Noto Color Emoji" ];
+      #     monospace = [ "FreeMono" ];
+      #     sansSerif = [ "FreeSans" ];
+      #     serif = [ "FreeSerif" ];
+      #   };
+    };
     packages = with pkgs; [
       noto-fonts-emoji
 
+      agave
       cascadia-code
       corefonts
       creep
